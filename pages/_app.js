20e24cefd75cadb499b3a4a6412a8a1c19ac5563
Amelia <amelia.rae27@gmail.com>
2023-08-04 14:24:04 -0700

    trying to add the commandbar in app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,26 @@
+import { useEffect, useState } from "react";
+import { useRouter } from "next/router";
+import { AppProps } from "next/app";
+import { init } from "commandbar";
+
 import '@styles/globals.css'
 
+if (typeof window !== "undefined") {
+  init('81136260');
+}
+
 function Application({ Component, pageProps }) {
+
+  useEffect(() => {
+    const loggedInUserId = '12345'; // example
+    window.CommandBar.boot(loggedInUserId).then(() => {
+      setCommandBarReady(true);
+    });
+    return () => {
+      window.CommandBar.shutdown();
+    }
+  }, []);
+
   return <Component {...pageProps} />
 }
 
