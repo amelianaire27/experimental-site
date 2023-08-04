37f704edcea21e4914bbabe811735fdf349cdf56
Amelia <amelia.rae27@gmail.com>
2023-08-04 13:33:08 -0700

    file installation instead of in-app

--- pages/about.js
+++ pages/about.js
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
 
+
 export default function About() {
   return (
     <div className="container">
