f303024fb757b0c907cf8d83db5bacb218938984
Amelia <amelia.rae27@gmail.com>
2023-08-04 19:07:46 -0700

    removed unused reference

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,6 @@ import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
 import Button from '../components/Button';
-import { fetchCatFact } from '../utils/api';
 
 export default function Home() {
   const handleButtonClick = () => {
