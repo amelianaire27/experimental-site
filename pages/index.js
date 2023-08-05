2976424ef504af27d902b30e60ce9896b32a8aff
Amelia <amelia.rae27@gmail.com>
2023-08-04 19:06:41 -0700

    Once more

--- pages/index.js
+++ pages/index.js
@@ -2,14 +2,11 @@ import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
 import Button from '../components/Button';
+import { fetchCatFact } from '../utils/api';
 
 export default function Home() {
   const handleButtonClick = () => {
     window.CommandBar.trackEvent("buttonClicked", {});
-  };
-  const onSearchCats = () => {
-    return fetch(`https://catfact.ninja/fact`)
-    .then(response => response.json())
   };
   return (
     <div className="container">
